eeab6b05bc1fcfca2055be39925fead0139bc8c6
Jhersonn Cayao <jvcayao@outlook.com>
2022-04-20 02:23:54 +0800

    First Commit

---
+++ assets/img/award/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<?php
+	require_once("../../includes/initialize.php");
+	include_layout_template("header.php");
+?>
+        <!--Header End-->
+        <section class="errorPage">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2 col-xs-12 text-center noPadding">
+                        <div class="errorIn">
+                            <h1>404</h1>
+                            <h2>The page you're looking for could not be found.</h2>
+                            <p>
+                                The page you're looking for could not be found.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<?php
+	include_layout_template("footer.php");
+?>
+    </body>
+</html>
